6d7a81e564cded7af6f4bd0dd5ae5afc0b1a40a2
Baptiste Boussemart <baptiste.boussemart@consensys.net>
2022-01-19 15:50:59 +0100

    local changes

---
+++ examples/7nodes/setenv.sh
@@ -0,0 +1,4 @@
+export GETHDIR="/home/baptiste/go/src/github.com/ethereum/go-ethereum"
+export PATH=$GETHDIR/build/bin:$PATH                                  
+export TESSERA_SCRIPT=~/tessera-21.7.1/bin/tessera
+export CAKESHOP_JAR=/home/baptiste/cakeshop-0.12.1.war
